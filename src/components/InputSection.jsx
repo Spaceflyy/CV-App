9f5ac9ea1bb4c50bc52521be2a9f804e885b800f
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-12-30 16:44:04 +0000

    add rotating chevron when section is expanded

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -1,33 +1,36 @@
 import { useState } from "react";
 
 export default function InputSection({ title, fields, collapseable }) {
-	const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(collapseable);
 
 	const toggle = () => {
 		setOpen(!open);
 	};
+
 	return (
 		<div className="inputSection">
-			<h2>
-				{title}{" "}
-				{collapseable ? <span onClick={toggle} className="chevron"></span> : ""}
+			<h2 onClick={toggle}>
+				{title}
+				{collapseable ? (
+					<span className={open ? "chevron" : "chevron after"}></span>
+				) : (
+					""
+				)}
 			</h2>
-			{open && (
-				<form action="">
-					{fields.map((field) => {
-						return (
-							<label key={field.id} htmlFor={field.name}>
-								<h3>{field.name}</h3>
-								{field.type === "textarea" ? (
-									<textarea id={field.name} type={field.type} />
-								) : (
-									<input id={field.name} type={field.type} />
-								)}
-							</label>
-						);
-					})}
-				</form>
-			)}
+			<form className={open && "hidden"} action="">
+				{fields.map((field) => {
+					return (
+						<label key={field.id} htmlFor={field.name}>
+							<h3>{field.name}</h3>
+							{field.type === "textarea" ? (
+								<textarea id={field.name} type={field.type} />
+							) : (
+								<input id={field.name} type={field.type} />
+							)}
+						</label>
+					);
+				})}
+			</form>
 		</div>
 	);
 }
